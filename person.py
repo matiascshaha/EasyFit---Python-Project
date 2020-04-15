761bbef3254bdc6846cc466b33f4dab7d74951b4
ransell98 <61478817+ransell98@users.noreply.github.com>
2020-04-15 17:32:18 -0400

    Update person.py

--- person.py
+++ person.py
@@ -1,14 +1,16 @@
-class person:
-    def __init__(self,height,weight,gender,objective):​
-        self.height = height​
-        self.weight = weight​
-        self.gender = gender​
-        self.objective = objective          #to lose weight or gain muscle​
-    def getHeight(self):​
-        return self.height​
-    def getWeight(self):​
-        return self.weight​
-    def getGender(self):​
-        return self.gender​
-    def getObjective(self):​
-        return self.objective l
+class Person:
+    pass
+
+    # def __init__(self,height,weight,gender,objective):​
+    #     self.height = height​
+    #     self.weight = weight​
+    #     self.gender = gender​
+    #     self.objective = objective          #to lose weight or gain muscle​
+    # def getHeight(self):​
+    #     return self.height​
+    # def getWeight(self):​
+    #     return self.weight​
+    # def getGender(self):​
+    #     return self.gender​
+    # def getObjective(self):​
+    #     return self.objective l
