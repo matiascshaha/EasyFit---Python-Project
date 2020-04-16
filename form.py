a733c9ccd7c926632d96494728023ee0e286f195
tjgarcia0427 <42558729+tjgarcia0427@users.noreply.github.com>
2020-04-16 19:39:03 -0400

    User input is stored

--- form.py
+++ form.py
@@ -13,39 +13,50 @@ class EasyFitWindow(QDialog):
 
     def __init__(self):
         super(EasyFitWindow, self).__init__()
+        self.hbox = QHBoxLayout()
+        self.nameLabel = QLineEdit()
+        self.heightLabel = QLineEdit()
+        self.weightLabel = QLineEdit()
+        self.goal = QLineEdit()
+        self.r1 = QRadioButton("Male")
+        self.r2 = QRadioButton("Female")
         self.createFormGroupBox()
-
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
-
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.formGroupBox)
         mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
-
         self.setWindowTitle("EazyFit Window")
 
     def createFormGroupBox(self):
         self.formGroupBox = QGroupBox("Form Layout")
         layout = QFormLayout()
-        hbox = QHBoxLayout()
-        nameLabel = QLineEdit()
-        heightLabel = QLineEdit()
-        goal = QLineEdit()
-        r1 = QRadioButton("Male")
-        r2 = QRadioButton("Female")
-        hbox.addWidget(r1)
-        hbox.addWidget(r2)
-        hbox.addStretch()
-        layout.addRow(QLabel("Name:"), nameLabel)
-        layout.addRow(QLabel("Height:"), heightLabel)
-        layout.addRow(QLabel("Workout Goal:"), goal)
-        layout.addRow(QLabel("Gender:"), hbox)
+        self.hbox.addWidget(self.r1)
+        self.hbox.addWidget(self.r2)
+        self.hbox.addStretch()
+        layout.addRow(QLabel("Name:"), self.nameLabel)
+        layout.addRow(QLabel("Height:"), self.heightLabel)
+        layout.addRow(QLabel("Weight:"), self.weightLabel)
+        layout.addRow(QLabel("Workout Goal:"), self.goal)
+        layout.addRow(QLabel("Gender:"), self.hbox)
         self.formGroupBox.setLayout(layout)
 
+    def accept(self):
+        n = self.nameLabel.text()
+        h = self.heightLabel.text()
+        w = self.weightLabel.text()
+        g = self.goal.text()
+        if self.r1.isChecked():
+            gen = "M"
+        else:
+            gen = "F"
+        # h, g, w, and gen each hold the user input height, goal, weight and gender as a str
+        print(w)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     eazyFit = EasyFitWindow()
     sys.exit(eazyFit.exec())
+
