72d5c96f0c9363fceb59c70ce636b121fe75c288
Michael Styron <mts17b@my.fsu.edu>
2020-04-22 21:33:08 -0400

    Updating Database.py and setup.py
    -Added support for age
    -Added a check function to see if a user has been previously added (returns true and false values)

--- Database.py
+++ Database.py
@@ -5,9 +5,9 @@ class Database():
         self.db = sqlite3.connect('database.db')
         self.cursor = self.db.cursor()
 
-    def newUser(self,name1,height1,weight1,gender1):
-        self.cursor.execute('''INSERT INTO person(name,height,weight,gender)
-                          VALUES(?,?,?,?)''', (name1,height1,weight1,gender1,))
+    def newUser(self,name1,age1,height1,weight1,gender1):
+        self.cursor.execute('''INSERT INTO person(name,age,height,weight,gender)
+                          VALUES(?,?,?,?,?)''', (name1,age1,height1,weight1,gender1,))
         self.db.commit()
 
     def deleteUser(self,name1):
@@ -23,6 +23,13 @@ class Database():
                           (weight1,name1,))
         self.db.commit()
         return weightChange
+
+    def check(self,name1):
+        self.cursor.execute('''SELECT id FROM person WHERE name = ?''', (name1,))
+        if self.cursor.fetchone() is None:
+            return False
+        else:
+            return True
 
     def close(self):
         self.db.close()
