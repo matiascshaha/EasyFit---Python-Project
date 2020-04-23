d5c7191eb809208c42179b02f6d877458a003da1
Matias <mp17m@my.fsu.edu>
2020-04-22 21:36:27 -0400

    Diet class(tentative)

--- Diet.py
+++ Diet.py
@@ -5,6 +5,15 @@ class Diet:
         else:
             self.BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)
         self.Calorie_Calc = self.BMR * 1.55
+        self.protein_proportion = self.Calorie_Calc * .4
+        self.carb_proportion = self.Calorie_Calc * .4
+        self.fat_proportion = self.Calorie_Calc * .2
+
+    def print_Diet(self):
+        print("Total Calories: " + str(self.Calorie_Calc))
+        print("Calories of protein: " + str(self.protein_proportion) + " (" + str(self.protein_proportion /4) + "g)")
+        print("Calories of Carb: " + str(self.carb_proportion) + " (" + str(self.carb_proportion /4) + "g)")
+        print("Calories of Fat: " + str(self.fat_proportion) + " (" + str(self.fat_proportion /9) + "g)")
 
 
 
