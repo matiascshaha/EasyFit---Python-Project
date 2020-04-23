fbe97bae7ce5d275f89c261083762bc5fabd8c56
ransell98 <61478817+ransell98@users.noreply.github.com>
2020-04-23 18:03:54 -0400

    Update form.py

--- form.py
+++ form.py
@@ -56,7 +56,7 @@ class EasyFitWindow(QDialog):
         self.gbox.addWidget(self.goal3)
         self.gbox.addStretch()
 
-        layout.addRow(QLabel("Name:"), self.nameLabel)
+        layout.addRow(QLabel("Full Name:"), self.nameLabel)
         layout.addRow(QLabel("Age:"), self.ageLabel)
         layout.addRow(QLabel("Height:   (feet)"), self.feetLabel)
         layout.addRow(QLabel("          (inches)"), self.inchesLabel)
@@ -88,15 +88,17 @@ class EasyFitWindow(QDialog):
 
         text = None
         db = Database.Database()
-        if db.check(n) == True:
-            wChange = db.update(n,w)
+        if db.check(n):
+            wChange = db.update(n, w)
             if wChange >= 0:
-                text = "You have gained " + str(wChange) + " pounds.\n\n"
+                text = "Welcome back " + n + "! You have gained " + str(wChange) + " pounds. Here are your updated " \
+                                                                                   "diet and workout plans:\n\n "
             else:
                 wChange = str(wChange)
-                text = "You have lost " + wChange[1:] + " pounds.\n\n"
+                text = "Welcome back " + n + "! You have lost " + wChange[1:] + " pounds. Here are your updated diet " \
+                                                                                "and workout plans:\n\n "
         else:
-            text = "Welcome new user!\n\n"
+            text = "Welcome to EasyFit " + n + "! Here are your first diet and workout plans:\n\n"
             db.newUser(n, a, h, w, gen)
         db.close()
 
@@ -121,96 +123,3 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     eazyFit = EasyFitWindow()
     sys.exit(eazyFit.exec())
-
-
-# import sys
-
-# from PyQt5.QtWidgets import (QApplication, QDialog,
-#                              QDialogButtonBox, QFormLayout, QGroupBox, QHBoxLayout,
-#                              QLabel, QLineEdit, QVBoxLayout, QRadioButton, QButtonGroup)
-
-# import person
-
-
-# class EasyFitWindow(QDialog):
-#     numGridRows = 3
-#     numButtons = 4
-
-#     def __init__(self):
-#         super(EasyFitWindow, self).__init__()
-#         self.hbox = QHBoxLayout()
-#         self.nameLabel = QLineEdit()
-#         self.ageLabel = QLineEdit()
-#         self.feetLabel = QLineEdit()
-#         self.inchesLabel = QLineEdit()
-#         self.weightLabel = QLineEdit()
-#         self.gbox = QHBoxLayout()
-#         self.goal1 = QRadioButton("Weight Loss")
-#         self.goal2 = QRadioButton("Maintenance")
-#         self.goal3 = QRadioButton("Strength Gain")
-#         self.r1 = QRadioButton("Male")
-#         self.r2 = QRadioButton("Female")
-#         self.createFormGroupBox()
-#         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-#         buttonBox.accepted.connect(self.accept)
-#         buttonBox.rejected.connect(self.reject)
-#         mainLayout = QVBoxLayout()
-#         mainLayout.addWidget(self.formGroupBox)
-#         mainLayout.addWidget(buttonBox)
-#         self.setLayout(mainLayout)
-#         self.setWindowTitle("EasyFit")
-
-#     def createFormGroupBox(self):
-#         self.formGroupBox = QGroupBox()
-#         self.genGroup = QButtonGroup(self.formGroupBox)
-#         self.goalGroup = QButtonGroup(self.formGroupBox)
-#         self.genGroup.addButton(self.r1)
-#         self.genGroup.addButton(self.r2)
-#         self.goalGroup.addButton(self.goal1)
-#         self.goalGroup.addButton(self.goal2)
-#         self.goalGroup.addButton(self.goal3)
-#         layout = QFormLayout()
-#         self.hbox.addWidget(self.r1)
-#         self.hbox.addWidget(self.r2)
-#         self.hbox.addStretch()
-#         self.gbox.addWidget(self.goal1)
-#         self.gbox.addWidget(self.goal2)
-#         self.gbox.addWidget(self.goal3)
-#         self.gbox.addStretch()
-
-#         layout.addRow(QLabel("Name:"), self.nameLabel)
-#         layout.addRow(QLabel("Age:"), self.ageLabel)
-#         layout.addRow(QLabel("Height:   (feet)"), self.feetLabel)
-#         layout.addRow(QLabel("          (inches)"), self.inchesLabel)
-#         layout.addRow(QLabel("Weight:"), self.weightLabel)
-#         layout.addRow(QLabel("Workout Goal:"), self.gbox)
-#         layout.addRow(QLabel("Gender:"), self.hbox)
-#         self.formGroupBox.setLayout(layout)
-
-#     def accept(self):
-#         n = self.nameLabel.text()
-#         a = int(self.ageLabel.text())
-#         h = 12 * int(self.feetLabel.text()) + int(self.inchesLabel.text())
-#         w = int(self.weightLabel.text())
-#         if self.goal1.isChecked():
-#             g = 'C'
-#         elif self.goal2.isChecked():
-#             g = 'M'
-#         else:
-#             g = 'B'
-#         if self.r1.isChecked():
-#             gen = 'M'
-#         else:
-#             gen = 'F'
-
-#         p = person.Person(n, a, h, w, g, gen)
-#         print("\t\t", "DAILY DIET PLAN FOR ", n.upper(), ":")
-#         p.diet.print_Diet()
-#         print("\n\n\t\t", "WORKOUT PLAN FOR ", n.upper(), ":")
-#         p.workout.printWorkout()
-
-
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     eazyFit = EasyFitWindow()
-#     sys.exit(eazyFit.exec())
