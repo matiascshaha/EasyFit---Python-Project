7f43844e704366e84598017085f7be978f4878bf
ransell98 <61478817+ransell98@users.noreply.github.com>
2020-04-23 15:05:16 -0400

    Update person.py

--- person.py
+++ person.py
@@ -12,5 +12,21 @@ class Person:
         self.gender = gender
         self.workout = workout.workout(goal, gender)
         self.diet = Diet.Diet(weight, height, age, goal, gender)
+        
+        
+# import workout
+# import Diet
+
+
+# class Person:
+#     def __init__(self, name, age, height, weight, goal, gender):
+#         self.name = name
+#         self.age = age
+#         self.height = height
+#         self.weight = weight
+#         self.goal = goal
+#         self.gender = gender
+#         self.workout = workout.workout(goal, gender)
+#         self.diet = Diet.Diet(weight, height, age, goal, gender)
       
 
