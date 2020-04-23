8a4eef4ae8d9d2b07bad878744d261bab0e814ba
tjgarcia0427 <42558729+tjgarcia0427@users.noreply.github.com>
2020-04-22 20:01:16 -0400

    Adding radio buttons for Workout Goal question

--- form.py
+++ form.py
@@ -1,4 +1,94 @@
 from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
+                              QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
+                              QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
+                              QVBoxLayout, QRadioButton, QWidget,QButtonGroup)
+import person
+import workout
+import sys
+
+class EasyFitWindow(QDialog):
+    numGridRows = 3
+    numButtons = 4
+
+    def __init__(self):
+        super(EasyFitWindow, self).__init__()
+        self.hbox = QHBoxLayout()
+        self.nameLabel = QLineEdit()
+        self.heightLabel = QLineEdit()
+        self.weightLabel = QLineEdit()
+        self.gbox = QHBoxLayout()
+        # self.goal = QLineEdit()
+        self.goal1 = QRadioButton("Weight Loss")
+        self.goal2 = QRadioButton("Maintenance")
+        self.goal3 = QRadioButton("Strength Gain")
+        self.r1 = QRadioButton("Male")
+        self.r2 = QRadioButton("Female")
+        self.createFormGroupBox()
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formGroupBox)
+        mainLayout.addWidget(buttonBox)
+        self.setLayout(mainLayout)
+        self.setWindowTitle("EazyFit Window")
+
+    def createFormGroupBox(self):
+        self.formGroupBox = QGroupBox("Form Layout")
+        self.genGroup = QButtonGroup(self.formGroupBox)
+        self.goalGroup = QButtonGroup(self.formGroupBox)
+        self.genGroup.addButton(self.r1)
+        self.genGroup.addButton(self.r2)
+        self.goalGroup.addButton(self.goal1)
+        self.goalGroup.addButton(self.goal2)
+        self.goalGroup.addButton(self.goal3)
+        layout = QFormLayout()
+        self.hbox.addWidget(self.r1)
+        self.hbox.addWidget(self.r2)
+        self.hbox.addStretch()
+        self.gbox.addWidget(self.goal1)
+        self.gbox.addWidget(self.goal2)
+        self.gbox.addWidget(self.goal3)
+        self.gbox.addStretch()
+
+        layout.addRow(QLabel("Name:"), self.nameLabel)
+        layout.addRow(QLabel("Height:"), self.heightLabel)
+        layout.addRow(QLabel("Weight:"), self.weightLabel)
+        layout.addRow(QLabel("Workout Goal:"), self.gbox)
+        layout.addRow(QLabel("Gender:"), self.hbox)
+        self.formGroupBox.setLayout(layout)
+
+    def accept(self):
+        n = self.nameLabel.text()
+        h = self.heightLabel.text()
+        w = self.weightLabel.text()
+        if self.goal1.isChecked():
+            g = "C"
+        elif self.goal2.isChecked():
+            g = "M"
+        else:
+            g = "B"
+        if self.r1.isChecked():
+            gen = "M"
+        else:
+            gen = "F"
+         # h, g, w, and gen each hold the user input height, goal, weight and gender as a str
+        print(n)
+        print(h)
+        print(w)
+        print(g)
+        print(gen)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    eazyFit = EasyFitWindow()
+    sys.exit(eazyFit.exec())
+
+
+
+"""
+from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
                              QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
                              QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
                              QVBoxLayout, QRadioButton)
@@ -59,7 +149,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     eazyFit = EasyFitWindow()
     sys.exit(eazyFit.exec())
-
+"""
 
 # from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
 #                              QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
